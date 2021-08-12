9f4f8bb02d8e39dad8d41e1bbfcf59225773e2c5
velmurgan pillai <karthikpillai1323@gmail.com>
2021-08-12 12:53:53 +0530

    Designed Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,48 @@
 import React from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Notes Bucket</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="m-auto">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="mr-2"
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav style={{ maxHeight: "100px" }} navbarScroll>
+            <Nav.Link href="#action1">My Notes</Nav.Link>
+
+            <NavDropdown title="Velmurgan Pillai" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
+
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Logout</NavDropdown.Item>
+            </NavDropdown>
+            {/* <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
