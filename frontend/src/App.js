dd1f1ea9ba55a5435770d16fc1cbc18d7e679018
velmurgan pillai <karthikpillai1323@gmail.com>
2021-08-12 12:16:48 +0530

    Imported components to app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main style={{ minHeight: "93vh" }}></main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
