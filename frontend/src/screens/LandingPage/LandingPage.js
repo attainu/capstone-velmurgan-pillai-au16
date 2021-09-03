00b8acba63cd9f8e43dba3e7944170eacb7c16e9
velmurgan pillai <karthikpillai1323@gmail.com>
2021-09-03 11:02:42 +0530

    Multiple screens for the project

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,41 +1,49 @@
-import React from "react";
-import { Button, Container } from "react-bootstrap";
-import "./LandingStyle.css";
-const LandingPage = () => {
+import React, { useEffect } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./LandingStyles.css";
+
+function LandingPage({ history }) {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/mynotes");
+    }
+  }, [history, userInfo]);
+
   return (
     <div className="main">
       <Container>
-        <row>
+        <Row>
           <div className="intro-text">
             <div>
-              <h2 className="title">Welcome to Notes Bucket</h2>
-              <p className="subtitle">One Safe place for all your notes</p>
+              <h1 className="title">Welcome to Note Zipper</h1>
+              <p className="subtitle">One Safe place for all your notes.</p>
             </div>
             <div className="buttonContainer">
-              <a href="/login">
-                <Button
-                  size="lg"
-                  className="landing-btn"
-                  variant="outline-primary"
-                >
+              <Link to="/login">
+                <Button size="lg" className="landingbutton">
                   Login
                 </Button>
-              </a>
-              <a href="/login">
+              </Link>
+              <Link to="/register">
                 <Button
-                  size="lg"
-                  className="landing-btn1"
                   variant="outline-primary"
+                  size="lg"
+                  className="landingbutton"
                 >
                   Signup
                 </Button>
-              </a>
+              </Link>
             </div>
           </div>
-        </row>
+        </Row>
       </Container>
     </div>
   );
-};
+}
 
 export default LandingPage;
